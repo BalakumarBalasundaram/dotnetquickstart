bf0e0705fd28e6c16a35f1563981557e102f31c1
Balakumar Balasundaram <balakumar.b05@gmail.com>
2022-09-18 18:29:55 +0200

    added Kafka Consumer app

---
+++ src/KafkaCache.CacheBuilder/KafkaConsumer.cs
@@ -0,0 +1,57 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaCache.CacheBuilder
+{
+    class KafkaConsumer
+    {
+         static async Task Main(string[] args)
+        {
+            var consumerConfig = new ConsumerConfig
+            {
+                GroupId = "products.cache.builder.group.id",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+            };
+
+            var producerConfig = new ProducerConfig { BootstrapServers = "localhost:9092" };
+            var cacheTopic = "products.cache";
+
+            using (var c = new ConsumerBuilder<string, string>(consumerConfig).Build())
+            {
+                c.Subscribe("mysql.mystore.products");
+                try
+                {
+                    using (var p = new ProducerBuilder<int, string>(producerConfig).Build())
+                    {
+                        while (true)
+                        {
+                            try
+                            {
+                                var cr = c.Consume();
+                                var key = JsonConvert.DeserializeObject<ProductKey>(cr.Key);
+
+                                await p.ProduceAsync(cacheTopic, new Message<int, string> { Value = cr.Value, Key = key.Id });
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e);
+                                throw e;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+        }
+    }
+}
--- src/KafkaCache.CacheBuilder/Program.cs
+++ src/KafkaCache.CacheBuilder/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace KafkaCache.CacheBuilder
+{
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
